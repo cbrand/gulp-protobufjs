e39e6a97af55dac06945c93b924947de4ce8b031
Christoph Brand <christoph@brand.rest>
2015-06-07 19:47:13 +0200

    gulpfile: added generated files from coverage data and testresults from coverage and linter information.

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,45 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
+var sourceFiles = [
+    'gulpfile.js',
+    '**/*.js',
+    '!node_modules/**/*.js',
+    '!testresults/**/*.js',
+    '!coverage/**/*.js'
+];
 
 gulp.task('lint:js', function () {
-    return gulp.src([
-        'gulpfile.js',
-        '**/*.js',
-        '!node_modules/**/*.js'
-    ]).pipe(jscs())
+    return gulp.src(sourceFiles)
+        .pipe(jscs())
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('mocha:coverage', function (done) {
-    gulp.src([
-        '!gulpfile.js',
-        '**/*.js',
-        '!node_modules/**/*.js'
-    ]).pipe(istanbul())
+    gulp.src(sourceFiles)
+        .pipe(istanbul())
         .pipe(istanbul.hookRequire())
         .on('finish', function () {
             gulp.src('test/**/*.js')
                 .pipe(mocha())
                 .pipe(istanbul.writeReports())
-                .pipe(istanbul.enforceThresholds({thresholds: {global: 90}}))
+                .pipe(istanbul.enforceThresholds({
+                    thresholds: {
+                        global: 90
+                    }
+                }))
                 .on('end', done)
             ;
         });
 });
 
-gulp.task('mocha:junit', function(done) {
-    gulp.src([
-        '!gulpfile.js',
-        '**/*.js',
-        '!node_modules/**/*.js'
-    ]).pipe(istanbul())
+gulp.task('mocha:junit', function (done) {
+    gulp.src(sourceFiles)
+        .pipe(istanbul())
         .pipe(istanbul.hookRequire())
-        .on('finish', function() {
+        .on('finish', function () {
             return gulp.src('test/**/*.js')
                 .pipe(mocha({
                     reporter: 'mocha-junit-reporter',
