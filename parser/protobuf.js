66debf353e75cc2eb5ccf3999aadfe8e3e660352
Martin Bonetti <martin.bonetti@omicron.at>
2016-01-08 10:45:08 +0100

    avoid importing files twice, fixes "Duplicate name in namespace" bug
    updated logic too the current pbjs implementation

--- parser/protobuf.js
+++ parser/protobuf.js
@@ -5,10 +5,8 @@
  */
 var fs = require('fs');
 var path = require('path');
-var nodePath = path;
 var ProtoBuf = require('protobufjs');
 var util = require('protobufjs/cli/pbjs/util');
-
 var optionsHandler = require('../options');
 var parseProtobufString;
 var parseProtobuf;
@@ -16,6 +14,11 @@ var parseProtobuf;
 parseProtobuf = function (protoBufPath, options, loaded) {
     options = optionsHandler.verify(options);
     protoBufPath = path.resolve(protoBufPath);
+    if (loaded.indexOf(protoBufPath) >= 0)
+        return {};
+    else {
+        loaded.push(protoBufPath)
+    }
     var data = fs.readFileSync(protoBufPath, options.encoding || 'utf-8');
     return parseProtobufString(data, options, loaded);
 };
@@ -24,30 +27,25 @@ parseProtobufString = function (protoBufString, options, loaded) {
     options = optionsHandler.verify(options);
     var parser = new ProtoBuf.DotProto.Parser(protoBufString);
     var data = parser.parse();
-    loaded = loaded || {};
-    if (Array.isArray(data.imports)) {
-        var imports = data.imports;
-        for (var i = 0; i < imports.length; i++) {
+    loaded = loaded || [];
+    if (Array.isArray(data['imports'])) {
+        var imports = data['imports'];
+        for (var i=0; i<imports.length; i++) {
             // Skip pulled imports and legacy descriptors
-            if (typeof imports[i] !== 'string' || (util.isDescriptor(imports[i]) && !options.legacy)) {
+            if (typeof imports[i] !== 'string' || (util.isDescriptor(imports[i]) && !options.legacy))
                 continue;
-            }
             // Merge imports, try include paths
-            var fileFound = false;
-            var path = options.path || [];
-            for (var j = 0; j < path.length; ++j) {
-                var importFilename = nodePath.resolve(path[j] + '/', imports[i]);
-                if (!fs.existsSync(importFilename)) {
-                    continue;
+            (function() {
+                var path = options.path || [];
+                for (var j=0; j<path.length; ++j) {
+                    var import_filename = path.resolve(path[j] + "/", imports[i]);
+                    if (!fs.existsSync(import_filename))
+                        continue;
+                    imports[i] = parseProtobuf(import_filename, options, loaded);
+                    return;
                 }
-                imports[i] = parseProtobuf(importFilename, options, loaded);
-                fileFound = true;
-                break;
-            }
-            if (!fileFound) {
-                throw Error('File not found: ' + imports[i]);
-            }
-
+                throw Error("File not found: "+imports[i]);
+            })();
         }
     }
     return data;
